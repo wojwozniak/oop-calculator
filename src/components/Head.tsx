89c18259f9cfe69d0dda3dc5bac4b358942fe6af
Wojtek <pulchnymamut@gmail.com>
2023-05-08 17:42:33 +0200

    Update Head.tsx

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,6 +1,26 @@
+/*
+  Projekt końcowy z przedmiotu
+  Programowanie Obiektowe (sem. letni 2023)
+  Kalkulator
+  Wersja 1.0
+  Autorzy:
+  Anna Pierzchała, Wojciech Woźniak
+  --------------------------------------------
+    Komponent Head
+    Komponent wyświetlający nagłówek strony
+    (część strony ponad kalkulatorem)
+  --------------------------------------------
+  Użycie: main.tsx
+*/
+
+
+// Importowanie bibliotek stylu
 import '../index.css';
 
+// Definicja komponentu Head
 const Head:React.FunctionComponent = () => {
+
+    // Zwraca element h1 zawierający tytuł strony
     return (
         <h1 id="title">
             Kalkulator
