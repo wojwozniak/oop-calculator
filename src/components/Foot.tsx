67b1d25080634bdfdc32bc5f3fcce16d428594b7
Wojtek <pulchnymamut@gmail.com>
2023-05-08 17:04:21 +0200

    update

--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -3,8 +3,7 @@ import '../index.css';
 const Foot:React.FunctionComponent = () => {
   return (
     <div id="foot">
-        <p id="subtitle">Made by <a href="https://twitter.com/woj_wozniak" target="_blank">woj.wozniak</a> for freecodecamp.org course</p>
-        <p id="more"><a href="https://wojwozniak.github.io/" target="_blank">More projects</a></p>
+        <p id="subtitle">Projekt końcowy z przedmiotu Programowanie Obiektowe</p>
     </div>
   );
 }
