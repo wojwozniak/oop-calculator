983b4570aacd0c49dad13c4d7edca861d1c254e7
Wojtek <pulchnymamut@gmail.com>
2023-05-08 17:42:31 +0200

    Update Foot.tsx

--- src/components/Foot.tsx
+++ src/components/Foot.tsx
@@ -1,6 +1,27 @@
+/*
+  Projekt końcowy z przedmiotu
+  Programowanie Obiektowe (sem. letni 2023)
+  Kalkulator
+  Wersja 1.0
+  Autorzy:
+  Anna Pierzchała, Wojciech Woźniak
+  --------------------------------------------
+    Komponent Foot
+    Komponent wyświetlający stopkę strony
+    (część strony pod kalkulatorem)
+  --------------------------------------------
+  Użycie: main.tsx
+*/
+
+
+
+// Importowanie bibliotek stylu
 import '../index.css';
 
+// Definicja komponentu Foot
 const Foot:React.FunctionComponent = () => {
+
+  // Zwraca element p zawierający podtytuł strony
   return (
     <div id="foot">
         <p id="subtitle">Projekt końcowy z przedmiotu Programowanie Obiektowe</p>
