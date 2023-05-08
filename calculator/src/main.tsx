3ced3d44781d6ac1a75347d8c636b98cc64c1894
Wojtek <pulchnymamut@gmail.com>
2023-05-08 16:14:29 +0200

    Restructured app

--- calculator/src/main.tsx
+++ calculator/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './calculator/App';
+import Head from './components/Head';
+import Foot from './components/Foot';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
+    <Head />
     <App />
-  </React.StrictMode>,
-)
+    <Foot />
+  </React.StrictMode>
+);
