e646b723aeb28c6520bb8de3c91ea613c0477456
Wojtek <pulchnymamut@gmail.com>
2023-05-08 16:40:50 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './calculator/App';
-import Test from './Test';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>,
 )
