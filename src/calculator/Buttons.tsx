55b1d839d0a526ecf68ab02b8022e303a0d3f1f0
Wojtek <pulchnymamut@gmail.com>
2023-05-08 18:04:17 +0200

    Create Buttons.tsx

---
+++ src/calculator/Buttons.tsx
@@ -0,0 +1,48 @@
+/*
+  Projekt końcowy z przedmiotu
+  Programowanie Obiektowe (sem. letni 2023)
+  Kalkulator
+  Wersja 1.0
+  Autorzy:
+  Anna Pierzchała, Wojciech Woźniak
+  --------------------------------------------
+    Komponent Buttons
+    Komponent odpowiedzialny za wyrenderowanie
+    przycisków i przekazywanie kliknięć do
+    komponentu Calculator
+  --------------------------------------------
+  Użycie: komponent Calculator
+*/
+
+
+// Importy
+import React from 'react'
+import Button from './Button';
+import buttons from './buttonList.json'
+
+
+// Komponent Buttons (przyjmuje jeden argument - funkcję dispatchReducer odpowiadającą za przekazywanie kliknięć do komponentu Calculator)
+const Buttons = ({dispatchReducer} : {dispatchReducer:Function}) => {
+
+    
+
+    // Każdy przycisk renderuje się jako element JSX
+    // z odpowiednią nazwą, kluczem, wyrenderowanym symbolem i przypisaniem funkcji dispatchReducer
+    return (
+        <div className="button-container">
+            {buttons.map((button) => {
+                return <Button 
+                    key={button.name} 
+                    name={button.name} 
+                    passData={
+                        (e:React.MouseEvent<HTMLButtonElement>) => 
+                        dispatchReducer(e)
+                    } 
+                    symbol={button.symbol} 
+                />
+            })}
+        </div>
+    )
+}
+
+export default Buttons
